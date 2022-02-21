3590f3315eb4b6e4c29b95528c182b241f29fdeb
Gregg Dent <71772319+dentnexus7@users.noreply.github.com>
2022-02-21 16:20:47 -0700

    Deploy to Rinkeby using HDWalletProvider and Verify via Flattener

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,10 @@
 require('@babel/register');
 require('@babel/polyfill');
+require("dotenv").config({path: "./.env"});
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const AccountIndex = 0;
+
 
 module.exports = {
   contracts_directory: './src/contracts/',
@@ -9,6 +14,12 @@ module.exports = {
      host: "127.0.0.1",
      port: 7545,
      network_id: "*"
+    },
+    rinkeby_infura: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`, AccountIndex)
+      },
+      network_id: 4,
     }
   },
   compilers: {
